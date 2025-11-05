d4ce002a413dfa94f6cc10770260de28a98df6ec
zbass90 <smilejkh90@gmail.com>
2025-11-05 15:22:21 +0900

    feat: spring-security init

---
+++ src/main/java/io/security/spring/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
